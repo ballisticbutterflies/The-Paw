3914bd2c1488cf40f9e9d43d849a13fbd5855605
Carmen <carmenshiu07@gmail.com>
2024-03-16 10:53:15 -0700

    working on adding review_images

--- app/api/businesses_routes.py
+++ app/api/businesses_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint;
-from app.models import Business;
+from app.models import Business, Review, Image;
 
 businesses_route = Blueprint('businesses', __name__)
 
@@ -8,5 +8,41 @@ def get_business(id):
     """
     Query for a business by id and returns that business in a dictionary
     """
+    business_data = []
+    review_images = {}
+
     business = Business.query.get(id)
-    return business.to_dict()
+    reviews = Review.query.filter(Review.business_id == id).all()
+    business_images = Image.query.filter(Image.imageable_id == id and Image.imageable_type == 'business').all()
+
+    for business_image in business_images:
+        business_image_url = business_image.url
+
+    total_stars = 0
+    num_reviews = len(reviews)
+
+    for review in reviews:
+        total_stars += review.stars
+        review_images = Image.query.filter(Image.imageable_id == review.id).all()
+        for image in review_images:
+            image_id = image.id
+            image_url = image.url
+    
+    avg_stars = total_stars / num_reviews
+
+    business_dict = business.to_dict()
+    print(review_images)
+    business_dict['reviews'] = {
+        'num_reviews': num_reviews,
+        'avg_stars': avg_stars,
+    }
+    business_dict['review_images'] = {
+        'image_id': image_id,
+        'image_url': image_url
+    }
+    # review_images['review_images'] = image_url
+    business_dict['business_images'] = business_image_url
+    
+    
+    business_data.append(business_dict)
+    return { 'business': business_data }
