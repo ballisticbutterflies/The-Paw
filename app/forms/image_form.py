3c93fb7e1b667247026ddf925ef7cfd0b00587a0
bssam <bssammel@gmail.com>
2024-03-18 20:55:14 -0400

    Works on my machine lol, installed boto at root

--- app/forms/image_form.py
+++ app/forms/image_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import SubmitField
-from app.api.utils.aws import ALLOWED_EXTENSIONS
+from app.utils.aws import ALLOWED_EXTENSIONS
 
 class ImageForm(FlaskForm):
     image = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
