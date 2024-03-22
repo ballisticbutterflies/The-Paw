2fe8c380305fb5a7a324cf6cba993e5ce1328341
bssam <bssammel@gmail.com>
2024-03-22 01:39:26 -0400

    breaking for night, need to talk to others about post body through post man

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .image_form import ImageForm
 from .business_form import CreateBusinessForm
+from .review_form import CreateReviewForm
