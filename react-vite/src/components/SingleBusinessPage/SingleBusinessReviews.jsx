10f73907eb02f32c789954d67680ccbaea787d33
Carmen <carmenshiu07@gmail.com>
2024-03-21 17:06:03 -0700

    working on reviews

--- react-vite/src/components/SingleBusinessPage/SingleBusinessReviews.jsx
+++ react-vite/src/components/SingleBusinessPage/SingleBusinessReviews.jsx
@@ -9,17 +9,24 @@ function SingleBusinessReviews({ businessId }) {
         if (a.createdAt < b.createdAt) return 1;
         return 0;
     });
-
     useEffect(() => {
         dispatch(getBusinessReviews(businessId))
     }, [dispatch, businessId])
+
+    const lastInitial = (lastName) => {
+        let last = lastName.charAt(0)
+        return last + "."
+    }
 
     return (reviews &&
         <>
             <h3>Reviews</h3>
             {reviews.map((review) => (
                 <div key={review.id}>
-                    {review.first_name}
+                    {review.user?.first_name} {review.user.last_name && lastInitial(review.user.last_name)}
+                    <div>{review.created_at}</div>
+
+                    <div>{review.review}</div>
                 </div>
             ))}
         </>
