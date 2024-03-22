ca6bbc9b0856b8facd6ae3941a2a2ee35a13a67a
Carmen <carmenshiu07@gmail.com>
2024-03-21 23:41:40 -0700

    added more to reviews

--- react-vite/src/components/SingleBusinessPage/SingleBusinessReviews.jsx
+++ react-vite/src/components/SingleBusinessPage/SingleBusinessReviews.jsx
@@ -18,15 +18,41 @@ function SingleBusinessReviews({ businessId }) {
         return last + "."
     }
 
+    const reviewStars = (numStars) => {
+        let filled_paws = [];
+        let unfilled_paws = []
+
+        for (let i = 0; i < numStars; i++) {
+            filled_paws.push(<span className="paws-filled" style={{ fontSize: "medium" }}><i className="fa-solid fa-paw"></i> </span>)
+        }
+
+        let remaining_paws = 5 - filled_paws.length
+
+        for (let i = 0; i < remaining_paws; i++) {
+            unfilled_paws.push(<span className="paws-unfilled" style={{ fontSize: "medium" }}><i className="fa-solid fa-paw"></i> </span>)
+        }
+
+        return [filled_paws, unfilled_paws]
+    }
+
     return (reviews &&
         <>
             <h3>Reviews</h3>
             {reviews.map((review) => (
                 <div key={review.id}>
-                    {review.user?.first_name} {review.user.last_name && lastInitial(review.user.last_name)}
-                    <div>{review.created_at}</div>
+                    <div className="userInfo">
+                        <div className="avatar"><i className="fa-solid fa-circle-user" />
+                        </div>
+                        <div className="userName">{review.user?.first_name} {review.user.last_name && lastInitial(review.user.last_name)}</div>
+                        <div className="loc">City, State</div>
+                        <div className="stats">
+                            <span className="paws-unfilled" style={{ fontSize: "small" }}><i className="fa-solid fa-paw" /></span> XX &nbsp;&nbsp;
+                            <span className="paws-unfilled" style={{ fontSize: "small" }}><i class="fa-regular fa-image" /></span> XX</div>
+                    </div>
+                    <p>{review.stars && (reviewStars(review.stars))} &nbsp;&nbsp;XX, XX, XXXX</p>
 
-                    <div>{review.review}</div>
+                    <p>{review.review}</p>
+                    <br />
                 </div>
             ))}
         </>
