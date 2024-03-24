75a0b15233296a48175fbea54b48caefc8955052
Carmen <carmenshiu07@gmail.com>
2024-03-23 23:27:43 -0700

    added tips and css

--- react-vite/src/components/AddPhotosToBusiness/AddPhotosToBusiness.jsx
+++ react-vite/src/components/AddPhotosToBusiness/AddPhotosToBusiness.jsx
@@ -6,7 +6,6 @@ import { useModal } from "../../context/Modal";
 import "./AddPhotos.css"
 
 function AddPhotosToBusiness({ businessId, business }) {
-    console.log(businessId);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [image, setImage] = useState(null);
@@ -37,20 +36,51 @@ function AddPhotosToBusiness({ businessId, business }) {
     return (business &&
         <>
             <h1>{business.name}: Add Photos</h1>
+            <div>View all photos</div>
             {sessionUser &&
-                <form onSubmit={handleSubmit} encType="multipart/form-data">
-                    <input
-                        type="file"
-                        accept="image/*"
-                        onChange={(e) => setImage(e.target.files[0])}
-                    />
-                    {errors.image && <div className="errors">&nbsp;{errors.image}</div>}
-                    {(imageLoading) && <p>Loading...</p>}
-                    <div>
-                        <button type="submit"
-                            disabled={!!Object.values(errors).length}>Upload photo</button>
+                <div className="addPhoto">
+                    <br />
+                    <form onSubmit={handleSubmit} encType="multipart/form-data" >
+                        <input
+                            type="file"
+                            accept="image/*"
+                            onChange={(e) => setImage(e.target.files[0])}
+                        />
+                        {errors.image && <div className="errors">&nbsp;{errors.image}</div>}
+                        {(imageLoading) && <p>Loading...</p>}
+                        <div>
+                            <button type="submit"
+                                disabled={!!Object.values(errors).length}>Upload photo</button>
+                        </div>
+                    </form>
+                    <br />
+                    <h2 style={{ textAlign: "center" }}>What Makes Paw-riffc Photos?
+                    </h2>
+                    <div className="tipSection">
+                        <div className="tip">
+                            <div className="icon"><i className="fa-solid fa-bullseye" /></div>
+                            <div>Photos are in focus. Shake off the blurry, shaky ones.</div>
+                        </div>
+
+
+                        <div className="tip">
+                            <div className="icon"><i className="fa-solid fa-sun" /></div>
+                            <div>Photos are well lit. We want to see those paws clearly!</div>
+                        </div>
+
+
+                        <div className="tip">
+                            <div className="icon"><i className="fa-solid fa-wand-sparkles" /></div>
+                            <div>Paw-mates, let&apos;s not overdo those filters.  </div>
+                        </div>
+
+
+                        <div className="tip">
+                            <div className="icon"><i className="fa-solid fa-store" /></div>
+                            <div>And of course, business photos only, please! </div>
+                        </div>
                     </div>
-                </form>
+                </div>
             }
         </>
     )
