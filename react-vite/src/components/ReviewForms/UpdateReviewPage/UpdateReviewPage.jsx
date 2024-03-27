60b0ae87481f21da5c11b38d29ba6397ada5668e
Carmen <carmenshiu07@gmail.com>
2024-03-27 13:51:55 -0700

    added useeffect and handlesubmit

--- react-vite/src/components/ReviewForms/UpdateReviewPage/UpdateReviewPage.jsx
+++ react-vite/src/components/ReviewForms/UpdateReviewPage/UpdateReviewPage.jsx
@@ -1,10 +1,35 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../../context/Modal";
+import { useState, useEffect } from "react";
 
 function UpdateReviewPage({ reviewId }) {
     const { closeModal } = useModal();
     const dispatch = useDispatch();
-    const reviewData = useSelector(state => state.reviews[reviewId])
+    const reviewData = useSelector(state => state.reviews[reviewId]);
+    const [review, setReview] = useState(reviewData?.review);
+    const [stars, setStars] = useState(reviewData?.stars);
+    const [hover, setHover] = useState(reviewData?.hover);
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        let errObj = {}
+        if (!review) errObj.review = "Review is required."
+        if (review && review.length < 85) errObj.review = "Reviews must be at least 85 characters in length.";
+        if (review && review.length > 2000) errObj.review = "Reviews must be 2000 characters in length at most.";
+        if (!stars) errObj.stars = "Paw Rating is required."
+        setErrors(errObj);
+    }, [review, stars])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors({});
+
+        const reviewData = {
+            review,
+            stars
+        }
+    }
+
     return "UPDATE"
 }
 
