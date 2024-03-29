cd42a7e81dfb9731f4b93c94767d2aae5f496ddf
traceybee23 <tracebee23@gmail.com>
2024-03-28 20:50:51 -0500

    fixed delete modal message when logged out

--- react-vite/src/components/DeleteImageModal/DeleteImageModal.jsx
+++ react-vite/src/components/DeleteImageModal/DeleteImageModal.jsx
@@ -9,29 +9,36 @@ function DeleteImageModal({ imageId, onlyImage }) {
     const dispatch = useDispatch();
     const { closeModal } = useModal();
 
-    const userId = useSelector(state => state.session.user.id)
+    const user = useSelector(state => state.session.user)
 
     const handleDelete = (e) => {
         e.preventDefault();
         dispatch(deleteImage(imageId))
-            .then(dispatch(getUserImages(userId)))
+            .then(dispatch(getUserImages(user.id)))
             .then(closeModal)
     }
 
 
     return (
         <>
-            {!onlyImage ? (<div className="deleteModal">
+            { user && !onlyImage ? (<div className="deleteModal">
                 <h1>Confirm Delete</h1>
                 <span>Are you sure you want to delete this photo?</span>
                 <button style={{ marginTop: "10px" }} onClick={handleDelete}>Yes, I&apos;m Sure (Delete Permanently)</button>
                 <button style={{ backgroundColor: "#c3cddf", color: "#768c9f", marginTop: "10px" }} onClick={closeModal}>No, Just Kitt-ing Around! (Keep Photo)</button>
             </div>)
                 :
-                (<div className="deleteModal">
+                (user && <div className="deleteModal">
                     <h1>Cannot Delete</h1>
                     <span>Businesses must have at least one photo uploaded. Please add another photo before deleting.</span>
                 </div>)
+            }
+            {
+                !user &&
+                <div className="deleteModal">
+                    <h1>Cannot Delete</h1>
+                    <span>You must be logged in to delete a photo.</span>
+                </div>
             }
         </>)
 }
