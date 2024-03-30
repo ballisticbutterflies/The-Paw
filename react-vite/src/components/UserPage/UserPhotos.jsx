888a02f917d16fe6154413ae4b05ce102f8e9570
Carmen <carmenshiu07@gmail.com>
2024-03-30 12:06:32 -0700

    user profile photos added goes to biz pages

--- react-vite/src/components/UserPage/UserPhotos.jsx
+++ react-vite/src/components/UserPage/UserPhotos.jsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector} from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getUser, getUserImages } from "../../redux/users";
 // import { getImagesByBusiness } from "../../redux/images";
@@ -11,6 +11,7 @@ import DeleteImageModal from "../DeleteImageModal/DeleteImageModal";
 function UserPhotos() {
     const { userId } = useParams();
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const sessionUser = useSelector(state => (
         state.session.user ? state.session.user : null
@@ -28,66 +29,68 @@ function UserPhotos() {
         state.users ? state.users.undefined : null
     ))
 
+    console.log(viewedUserImages);
+
     // console.log("viewed user images", viewedUserImages)
 
     const hasAtLeastOneImage = function () {
-        if(viewedUserImages != null){
-            if (!viewedUserImages.length <= 0){
+        if (viewedUserImages != null) {
+            if (!viewedUserImages.length <= 0) {
                 return true
-            } else {return 'No Images'}
-        } else{ return null}
+            } else { return 'No Images' }
+        } else { return null }
     }
 
     useEffect(() => {
         const runDispatches = async () => {
             await dispatch(getUser(parseInt(userId))).then(() =>
-              dispatch(getUserImages(parseInt(userId)))
+                dispatch(getUserImages(parseInt(userId)))
             );
-          };
-          runDispatches();
+        };
+        runDispatches();
     }, [dispatch, userId, sessionUser])
 
 
     return (
         <>
-           <h2>Photos</h2>
-           <div id="photos-container">
-                { viewedUserImages && hasAtLeastOneImage() == 'No Images' &&
-                    (<h4>Looks like this user does not have any images uploaded!</h4> )
+            <h2>Photos</h2>
+            <div id="photos-container">
+                {viewedUserImages && hasAtLeastOneImage() == 'No Images' &&
+                    (<h4>Looks like this user does not have any images uploaded!</h4>)
                 }
                 {viewedUserImages && hasAtLeastOneImage() && (
-                            viewedUserImages.map(user_image => (
-                                <>
-                                    <span key={user_image.id} className="allPhotosWrapper">
-                                        <img className="images"
-                                            src={user_image.image_url} />
-                                        <div className="photoCredit">
-                                            <div className="photoCreditText">
-                                                <div id="biz-name">
-                                                    &nbsp;&nbsp;{user_image.business_name}
-                                                </div>
-
-                                                <div className="trash">
-                                                    {((sessionUser && sessionUser.id === viewedUser.id )&& (typeof user_image.biz_images_count == "string" || user_image.biz_images_count > 1)) &&
-                                                        <OpenModalMenuItem
-                                                            itemText={<><i className="fa-solid fa-trash-can" style={{ color: "#FFFFFF" }} />&nbsp;&nbsp;</>}
-                                                            modalComponent={<DeleteImageModal imageId={user_image.id} />} />
-                                                    }
-                                                    {((sessionUser && sessionUser.id === viewedUser.id )&& (typeof user_image.biz_images_count == "number" && user_image.biz_images_count <= 1)) &&
-                                                        <OpenModalMenuItem
-                                                            itemText={<><i className="fa-solid fa-trash-can" style={{ color: "#FFFFFF" }} />&nbsp;&nbsp;</>}
-                                                            modalComponent={<DeleteImageModal imageId={user_image.id} onlyImage={true}/>} />
-                                                    }
-                                                </div>
-                                            </div>
+                    viewedUserImages.map(user_image => (
+                        <>
+                            <span key={user_image.id} className="allPhotosWrapper">
+                                <img className="images"
+                                    src={user_image.image_url} onClick={() => navigate(`/businesses/${user_image.type_id}`)} />
+                                <div className="photoCredit">
+                                    <div className="photoCreditText">
+                                        <div id="biz-name">
+                                            &nbsp;&nbsp;{user_image.business_name}
                                         </div>
-                                    </span >
-                                </>
-                            )
-                        )
+
+                                        <div className="trash">
+                                            {((sessionUser && sessionUser.id === viewedUser.id) && (typeof user_image.biz_images_count == "string" || user_image.biz_images_count > 1)) &&
+                                                <OpenModalMenuItem
+                                                    itemText={<><i className="fa-solid fa-trash-can" style={{ color: "#FFFFFF" }} />&nbsp;&nbsp;</>}
+                                                    modalComponent={<DeleteImageModal imageId={user_image.id} />} />
+                                            }
+                                            {((sessionUser && sessionUser.id === viewedUser.id) && (typeof user_image.biz_images_count == "number" && user_image.biz_images_count <= 1)) &&
+                                                <OpenModalMenuItem
+                                                    itemText={<><i className="fa-solid fa-trash-can" style={{ color: "#FFFFFF" }} />&nbsp;&nbsp;</>}
+                                                    modalComponent={<DeleteImageModal imageId={user_image.id} onlyImage={true} />} />
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            </span >
+                        </>
                     )
+                    )
+                )
                 }
-           </div>
+            </div>
         </>
     )
 }
