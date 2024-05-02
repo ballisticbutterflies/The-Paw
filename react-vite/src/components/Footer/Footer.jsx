4a3fe1d1d6850e74405dd9e289922c9f40b9801c
traceybee23 <tracebee23@gmail.com>
2024-05-01 20:14:11 -0500

    footer stuffs

---
+++ react-vite/src/components/Footer/Footer.jsx
@@ -0,0 +1,88 @@
+
+import './Footer.css'
+
+function Footer() {
+  return (
+    <>
+      <div className='footer'>
+        <img className='footer-image' src='../../images/footer.png' />
+        <div className='footer-heading'>
+          <div className="dev-heading" >Developers
+            <div className='devs'>
+              <div>
+                <a href='https://www.linkedin.com/in/carmenshiu/'>
+                  <span className="icons">
+                    <i className="fa-brands fa-linkedin fa-lg" />
+                  </span>
+                </a>&nbsp;&nbsp;
+                <a href="https://github.com/craftycarmen">
+                  <span className="icons">
+                    <i className="fa-brands fa-github fa-lg" />
+                  </span>
+                </a>
+                &nbsp;&nbsp;Carmen Shiu
+              </div>
+              <div>
+                <a href='https://www.linkedin.com/in/simon-sammel/'>
+                  <span className="icons">
+                    <i className="fa-brands fa-linkedin fa-lg" />
+                  </span>
+                </a>&nbsp;&nbsp;
+                <a href="https://github.com/bssammel">
+                  <span className="icons">
+                    <i className="fa-brands fa-github fa-lg" />
+                  </span>
+                </a>
+                &nbsp;&nbsp;Simon Sammel
+              </div>
+              <div>
+                <a href='https://www.linkedin.com/in/tracey-beard/'>
+                  <span className="icons">
+                    <i className="fa-brands fa-linkedin fa-lg" />
+                  </span>
+                </a>&nbsp;&nbsp;
+                <a href="https://github.com/traceybee23">
+                  <span className="icons">
+                    <i className="fa-brands fa-github fa-lg" />
+                  </span>
+                </a>
+                &nbsp;&nbsp;Tracey Beard
+              </div>
+            </div>
+          </div>
+          <div className='dev-heading'>Technologies
+            <div className='techs'>
+              <a href='https://docs.python.org/3/'>
+                <i className="fa-brands fa-python fa-lg" />
+              </a>
+              <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'>
+                <i className="fa-brands fa-js fa-lg" />
+              </a>
+              <a href='https://react.dev/'>
+                <i className="fa-brands fa-react fa-lg" />
+              </a>
+              <a href='https://www.w3.org/Style/CSS/Overview.en.html'>
+                <i className="fa-brands fa-css3-alt fa-lg" />
+              </a>
+            </div>
+          </div>
+          <div className='dev-heading'>Docs
+            <div className='docs'>
+              <a href='https://github.com/ballisticbutterflies/The-Paw'>
+                Repository
+              </a>
+              <a href='https://github.com/ballisticbutterflies/The-Paw/wiki'>
+                Wiki
+              </a>
+              <a href='https://www.canva.com/design/DAF-sAfmzW0/19OFQL2f7szQz104swZt0w/view?utm_content=DAF-sAfmzW0&utm_campaign=designshare&utm_medium=link&utm_source=editor'>
+                Wireframe
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer;
