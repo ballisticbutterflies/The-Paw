2abf7b90061240c2a7f8a7186558aef234c8922a
Carmen <carmenshiu07@gmail.com>
2024-05-02 16:25:11 -0700

    map fix

--- react-vite/src/components/SingleBusinessPage/BusinessMap.jsx
+++ react-vite/src/components/SingleBusinessPage/BusinessMap.jsx
@@ -1,8 +1,10 @@
-import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+// import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 import { fetchGeocode } from '../../redux/maps';
 import './SingleBusiness.css';
+import './BusinessMap.css';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Loader } from "@googlemaps/js-api-loader"
 
 function BusinessMap({ business }) {
     console.log("BUSINESS IN MAP", business);
@@ -27,22 +29,65 @@ function BusinessMap({ business }) {
         runDispatches()
     }, [dispatch, business.address, business.city, business.state])
 
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyBbMCWbhZBpVfKkdp8hqP5X6tt3BQMAdpo",
-    });
+    // const { isLoaded } = useLoadScript({
+    //     googleMapsApiKey: "AIzaSyBbMCWbhZBpVfKkdp8hqP5X6tt3BQMAdpo",
+    // });
 
-    const mapStyles = {
-        height: "200px",
-        width: "100%"
-    };
+    // const mapStyles = {
+    //     height: "200px",
+    //     width: "100%"
+    // };
 
-    const defaultCenter = {
-        lat: lat, lng: lng
-    };
+    // const defaultCenter = {
+    //     lat: lat, lng: lng
+    // };
+    // let map;
+
+    // const loader = new Loader({
+    //     apiKey: "AIzaSyBbMCWbhZBpVfKkdp8hqP5X6tt3BQMAdpo",
+    //     version: "weekly"
+    // });
+
+    // loader.load().then(async () => {
+    //     const { Map } = await google.maps.importLibrary("maps");
+
+    //     map = new Map(document.getElementById("map"), {
+    //         center: { lat: -34.397, lng: 150.644 },
+    //         zoom: 8,
+    //     });
+    // });
+    // Initialize and add the map
+    let map;
+
+    async function initMap() {
+        // The location of Uluru
+        const position = { lat: -25.344, lng: 131.031 };
+        // Request needed libraries.
+        //@ts-ignore
+        const { Map } = await google.maps.importLibrary("maps");
+        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+        // The map, centered at Uluru
+        map = new Map(document.getElementById("map"), {
+            zoom: 4,
+            center: position,
+            mapId: "DEMO_MAP_ID",
+        });
+
+        // The marker, positioned at Uluru
+        const marker = new AdvancedMarkerElement({
+            map: map,
+            position: position,
+            title: "Uluru",
+        });
+    }
+
+    initMap();
+
 
     return (
         <>
-            {!isLoaded ? (
+            {/* {!isLoaded ? (
                 <h1>Loading...</h1>
             ) : (
                 <GoogleMap
@@ -50,7 +95,10 @@ function BusinessMap({ business }) {
                     zoom={20}
                     center={defaultCenter}
                 />
-            )}
+            )} */}
+
+            <div id="map">
+            </div>
         </>
     )
 }
