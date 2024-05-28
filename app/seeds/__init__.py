52333d217096ef7ac0c9340a3fb6a7810564f461
traceybee23 <tracebee23@gmail.com>
2024-05-28 09:22:54 -0500

    added bizs seeds 4 import to init.py

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .aa_users import seed_users, undo_users
 from .businesses import seed_businesses, undo_businesses
 from .businesses_2 import seed_businesses_2, undo_businesses_2
+from .businesses_4 import seed_businesses_4, undo_businesses_4
 from .reviews import seed_reviews, undo_reviews
 from .images import seed_images, undo_images
 from .categories import seed_categories, undo_categories
@@ -34,6 +35,8 @@ def seed():
     print('######### businesses 3')
     seed_businesses()
     seed_businesses_2()
+    print('######### businesses TRACEY')
+    seed_businesses_4()
     print('######### reviews 4')
     seed_reviews()
     print('######### images 5')
@@ -47,6 +50,7 @@ def seed():
 def undo():
     undo_images()
     undo_reviews()
+    undo_businesses_4()
     undo_businesses_2()
     undo_businesses()
     undo_categories()
