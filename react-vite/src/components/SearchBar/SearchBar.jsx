395beaac08e139a2d0454438c8b74010f0d65e7d
traceybee23 <tracebee23@gmail.com>
2024-05-30 12:41:42 -0500

    search phrases fix

--- react-vite/src/components/SearchBar/SearchBar.jsx
+++ react-vite/src/components/SearchBar/SearchBar.jsx
@@ -30,32 +30,88 @@ const SearchBar = () => {
 
     if (searchQuery === 'restaurant' || searchQuery === 'restaurants') {
       queryParams.append('category', 1)
+      const queryString = queryParams.toString();
+      const url = `/search?${queryString}`;
+      dispatch(fetchBusinesses(searchQuery, location, {})).then(() => {
+        navigate(url)
+        setSearchQuery('')
+        setLocation('')
+      })
     }
     if (searchQuery === 'veterinarians' || searchQuery === 'veterinarian' || searchQuery === 'vet' || searchQuery === 'doctor') {
       queryParams.append('category', 2)
+      const queryString = queryParams.toString();
+      const url = `/search?${queryString}`;
+      dispatch(fetchBusinesses(searchQuery, location, {})).then(() => {
+        navigate(url)
+        setSearchQuery('')
+        setLocation('')
+      })
     }
     if (searchQuery === 'services' || searchQuery === 'groomer' || searchQuery === 'training' || searchQuery === 'walker') {
       queryParams.append('category', 3)
+      const queryString = queryParams.toString();
+      const url = `/search?${queryString}`;
+      dispatch(fetchBusinesses(searchQuery, location, {})).then(() => {
+        navigate(url)
+        setSearchQuery('')
+        setLocation('')
+      })
     }
     if (searchQuery === 'shopping' || searchQuery === 'store' || searchQuery === 'supplies' || searchQuery === 'boutique') {
       queryParams.append('category', 4)
+      const queryString = queryParams.toString();
+      const url = `/search?${queryString}`;
+      dispatch(fetchBusinesses(searchQuery, location, {})).then(() => {
+        navigate(url)
+        setSearchQuery('')
+        setLocation('')
+      })
     }
     if (searchQuery === 'travel' || searchQuery === 'supplies' || searchQuery === 'boutique') {
       queryParams.append('category', 5)
+      const queryString = queryParams.toString();
+      const url = `/search?${queryString}`;
+      dispatch(fetchBusinesses(searchQuery, location, {})).then(() => {
+        navigate(url)
+        setSearchQuery('')
+        setLocation('')
+      })
     }
-    if (searchQuery === 'activities' || searchQuery === 'park' || searchQuery === 'parks') {
+    if (searchQuery === 'activities'|| searchQuery === 'things to do' || searchQuery === 'park' || searchQuery === 'parks') {
       queryParams.append('category', 6)
+      const queryString = queryParams.toString();
+      const url = `/search?${queryString}`;
+      dispatch(fetchBusinesses(searchQuery, location, {})).then(() => {
+        navigate(url)
+        setSearchQuery('')
+        setLocation('')
+      })
     }
     if (searchQuery === 'adoption' || searchQuery === 'adopt') {
       queryParams.append('category', 7)
+      const queryString = queryParams.toString();
+      const url = `/search?${queryString}`;
+      dispatch(fetchBusinesses(searchQuery, location, {})).then(() => {
+        navigate(url)
+        setSearchQuery('')
+        setLocation('')
+      })
     }
     if (searchQuery === 'other' || searchQuery === 'photo' || searchQuery === 'photos' || searchQuery === 'burial') {
       queryParams.append('category', 8)
+      const queryString = queryParams.toString();
+      const url = `/search?${queryString}`;
+      dispatch(fetchBusinesses(searchQuery, location, {})).then(() => {
+        navigate(url)
+        setSearchQuery('')
+        setLocation('')
+      })
     }
 
+    if (!searchQuery) dispatch(fetchBusinesses()).then(() => { navigate('/search') })
     if (searchQuery) queryParams.append('search_query', searchQuery)
     if (location) queryParams.append('location', location)
-    if (!searchQuery) dispatch(fetchBusinesses()).then(() => { navigate('/search')})
     const queryString = queryParams.toString();
     const url = `/search?${queryString}`;
     console.log('Target URL SEARCH BAR :', url);
@@ -68,30 +124,30 @@ const SearchBar = () => {
 
   return (
     <div className="searchForm">
-    <form className="formNav" onSubmit={handleSubmit}>
-      <input
-        id="searchQuery"
-        type="text"
-        value={searchQuery}
-        placeholder="things to do, parks, restaurants"
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <input
-        id="location"
-        list="locations"
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
-        placeholder="city, state"
-      />
-      {/* <datalist id="locations">
+      <form className="formNav" onSubmit={handleSubmit}>
+        <input
+          id="searchQuery"
+          type="text"
+          value={searchQuery}
+          placeholder="things to do, parks, restaurants"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <input
+          id="location"
+          list="locations"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          placeholder="city, state"
+        />
+        {/* <datalist id="locations">
         {uniqueLocations.map(op => (
           <option key={op} value={op}>{op}</option>
         ))
         }
       </datalist> */}
-      <button id="search" type="submit"><i className="fa-solid fa-magnifying-glass" style={{ color: "#5f5ba8", fontSize: "large" }} /></button>
-    </form>
-  </div>
+        <button id="search" type="submit"><i className="fa-solid fa-magnifying-glass" style={{ color: "#5f5ba8", fontSize: "large" }} /></button>
+      </form>
+    </div>
   );
 };
 
