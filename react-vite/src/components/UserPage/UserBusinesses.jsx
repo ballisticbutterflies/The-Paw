d170183233beb8555b23427b6483f146e07229cd
Carmen <carmenshiu07@gmail.com>
2024-06-05 15:52:31 -0700

    ready for render

--- react-vite/src/components/UserPage/UserBusinesses.jsx
+++ react-vite/src/components/UserPage/UserBusinesses.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useDispatch, useSelector} from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getUser } from "../../redux/users";
 
@@ -7,14 +7,6 @@ import { getUser } from "../../redux/users";
 function UserBusinesses() {
     const { userId } = useParams();
     const dispatch = useDispatch();
-
-    const sessionUser = useSelector(state => (
-        state.session.user ? state.session.user : null
-    ))
-
-    const viewedUser = useSelector(state => (
-        state.users ? state.users[userId] : null
-    ))
 
     useEffect(() => {
         dispatch(getUser(parseInt(userId)))
