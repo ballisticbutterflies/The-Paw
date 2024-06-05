103bdf50ebb1970888869695ece74897611b38d6
Carmen <carmenshiu07@gmail.com>
2024-06-04 19:57:12 -0700

    done

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -94,9 +94,9 @@ def seed_reviews():
 
     for _ in range(1041):
         user_id=random.randint(51, 150)
-        business_id=random.randint(1, 24)
+        business_id=random.randint(1, 251)
         review=lorem.sentence()[:200]
-        stars=random.randint(1, 2)
+        stars=random.randint(1, 5)
         random_review=generate_review(user_id, business_id, review, stars)
         db.session.add(random_review)
 
@@ -110,7 +110,7 @@ def seed_reviews():
 
     for _ in range(2541):
         user_id=random.randint(51, 150)
-        business_id=random.randint(51, 102)
+        business_id=random.randint(25, 79)
         review=lorem.sentence()[:200]
         stars=random.randint(1, 2)
         random_review=generate_review(user_id, business_id, review, stars)
